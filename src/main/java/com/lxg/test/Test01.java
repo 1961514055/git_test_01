ed6568a5fa56bc91f6dc9d452491f203261112c1
git_test_01 <https://github.com/1961514055/git_test_01.git>
2020-12-01 11:53:56 +0800

    测试1

---
+++ src/main/java/com/lxg/test/Test01.java
@@ -0,0 +1,15 @@
+package com.lxg.test;
+
+/**
+ * @ClassName Test01
+ * @Description Git的学习测试
+ * @Author 刘晓鸽
+ * @Date 2020/12/1
+ * @Version V1.0.0
+ **/
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈");
+    }
+}
---
+++ src/test/java/com/lxg/test/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxg.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
